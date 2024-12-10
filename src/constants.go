9023f0a82af50a7f4269953905a6f5c45c561e68
BMilliet <brunomilliet89@gmail.com>
2024-12-10 21:11:19 +0000

    style(main): indent

--- src/constants.go
+++ src/constants.go
@@ -1,8 +1,8 @@
 package src
 
 const (
-	ExitSignal = "_quit_kcommit"
-	KcommitDirName = ".kcommit"
-	KcommitRcFileName = ".kcommitrc"
+	ExitSignal             = "_quit_kcommit"
+	KcommitDirName         = ".kcommit"
+	KcommitRcFileName      = ".kcommitrc"
 	KcommitHistoryFileName = ".kcommit_history.json"
 )
